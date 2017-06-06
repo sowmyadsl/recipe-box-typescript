00ce94389b5075909753eab108e1b71f617c7c6b
Sowmya Dinavahi and Tyler Stephenson <sowmya@>
2017-06-06 11:26:37 -0700

    add functionality to edit recipes and hide the edit button

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,15 +15,20 @@ import { Component } from '@angular/core';
           <li><strong>Ingredients:</strong> {{recipe.ingredients}}</li>
           <li><strong>Directions: </strong>{{recipe.directions}}</li>
         </ul>
-        <form>
-          <label name="edit-title">Edit title </label>
-          <input [(ngModel)]="recipe.title" [ngModelOptions]="{standalone: true}">
-          <label name="edit-ingredients">Edit ingredients</label>
-          <input [(ngModel)]="recipe.ingredients" [ngModelOptions]="{standalone: true}">
-          <label name="edit-directions">Edit directions</label>
-          <input [(ngModel)]="recipe.directions" [ngModelOptions]="{standalone: true}">
-          <button (click)="editRecipe()">Edit!</button>
-        </form>
+        <div *ngIf="recipe.editRecipe">
+            <br>
+            <label name="edit-title">Edit title </label>
+            <input [(ngModel)]="recipe.title">
+            <br>
+            <label name="edit-ingredients">Edit ingredients</label>
+            <input [(ngModel)]="recipe.ingredients">
+            <br>
+            <label name="edit-directions">Edit directions</label>
+            <input [(ngModel)]="recipe.directions">
+            <br>
+            <button (click)="editRecipe(recipe)">Done!</button>
+        </div>
+        <button *ngIf="recipe.editRecipe == false" (click)="editRecipe(recipe)">Edit</button>
       </div>
     </li>
   </ul>
@@ -31,7 +36,6 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  selectedRecipe: Recipe;
   recipes: Recipe[] = [
     new Recipe('Steak','Meat,potatoes,butter,rosemary, garlic, salt','Sear the meat and serve warm with mashed potatoes on the side'),
     new Recipe('Guacamole','Avacadoes,Jalapenos,Tomatoes,Onions,Cilantro,Salt,lemon','Cut the avacadoes and add diced onions,tomatoes,Jalapenos and salt and serve it chilled with cilantro on the top'),
@@ -41,10 +45,16 @@ export class AppComponent {
   onSelect(recipe){
     if (recipe.showDetails) {
       recipe.showDetails = false;
-      this.selectedRecipe = null;
     } else {
       recipe.showDetails = true;
-      this.selectedRecipe = recipe;
+    }
+  }
+
+  editRecipe(recipe){
+    if(recipe.editRecipe){
+      recipe.editRecipe = false;
+    } else {
+      recipe.editRecipe = true;
     }
   }
 
@@ -52,5 +62,6 @@ export class AppComponent {
 
 export class Recipe {
   showDetails: boolean = false;
+  editRecipe: boolean = false;
   constructor(public title: string, public ingredients: string, public directions: string){}
 }
